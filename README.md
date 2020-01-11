ee7a4b69c68d26ae146a2081547551c3b1309371
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-01-10 20:43:53 -0500

    finish the JS file code with store data in local storage

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Daily-Calendar
 
-Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+* ### Createing a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+* ### Useing the [Moment.js](https://momentjs.com/) library to work with date and time.
+
+* ### with Clicking on the save button will store the time and user input in `localStorage`.Near the top of the calendar, the application is display the current day. Additionally, each hour should be color coded to reflect whether the time slot is in the past *(`colored Red`)*, the present *(`colored Gray`)*, or the future *(`colored Green`)*. This will change depending on the time of day.
+
+
+* ### Using Boostrap library for styling.
--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 
 // console.log("moment",moment().hours());
 
+
 $(".saveBtn").on("click", save);
 
 function save() {
@@ -25,3 +26,39 @@ function save() {
   
   
 };
+
+// chaneg background color for each box base of local time
+$(".row").each(function(){
+    // console.log("each row",$(this).attr("id"));
+    if(parseInt($(this).attr("id")) < moment().hours()){
+      // console.log(" Time Past",$(this).attr("id"));
+      $(this).addClass("red");
+    }
+    else if(parseInt($(this).attr("id")) === moment().hours()){
+      // console.log(" Corrent Time",$(this).attr("id"));
+      $(this).addClass("gray");
+  
+  
+    }
+    else {
+      // console.log("future Time",$(this).attr("id"));
+      $(this).addClass("green");
+  
+    }
+  
+  });
+  // console.log("local storage 9am",localStorage.getItem("9 AM"));
+
+//   adding text to the app from local storage
+  $("#userInput-9").val(localStorage.getItem("9 AM"));
+  $("#userInput-10").val(localStorage.getItem("10 AM"));
+  $("#userInput-11").val(localStorage.getItem("11 AM"));
+  $("#userInput-12").val(localStorage.getItem("12 PM"));
+  $("#userInput-13").val(localStorage.getItem("1 PM"));
+  $("#userInput-14").val(localStorage.getItem("2 PM"));
+  $("#userInput-15").val(localStorage.getItem("3 PM"));
+  $("#userInput-16").val(localStorage.getItem("4 PM"));
+  $("#userInput-17").val(localStorage.getItem("5 PM"));
+  
+  $("#local-time").html(moment().format( 'llll'));
+  // console.log(moment());
