edab6e6e83bbce277f456afc078ceaf4dce59f41
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-01-17 01:24:23 -0500

    clear all consol.log

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// make sure js file is linked
-// console.log("this is js");
-// console.log("moment",moment().hours());
 
 
 $(".saveBtn").on("click", save);
@@ -9,55 +6,55 @@ function save() {
   var value = $(this)
     .siblings(".form")
     .val();
-  // console.log(value);
 
-  
+
+
 
   var time = $(this)
     .siblings(".time")
     .text();
-  // console.log(time);
 
-  
-  localStorage.setItem(time,value);
-  
 
-  
-  
+
+  localStorage.setItem(time, value);
+
+
+
+
 };
 
 // chaneg background color for each box base of local time
-$(".row").each(function(){
-    // console.log("each row",$(this).attr("id"));
-    if(parseInt($(this).attr("id")) < moment().hours()){
-      // console.log(" Time Past",$(this).attr("id"));
-      $(this).addClass("red");
-    }
-    else if(parseInt($(this).attr("id")) === moment().hours()){
-      // console.log(" Corrent Time",$(this).attr("id"));
-      $(this).addClass("gray");
-  
-  
-    }
-    else {
-      // console.log("future Time",$(this).attr("id"));
-      $(this).addClass("green");
-  
-    }
-  
-  });
-  // console.log("local storage 9am",localStorage.getItem("9 AM"));
+$(".row").each(function () {
+
+  if (parseInt($(this).attr("id")) < moment().hours()) {
+
+    $(this).addClass("red");
+  }
+  else if (parseInt($(this).attr("id")) === moment().hours()) {
+
+    $(this).addClass("gray");
+
+
+  }
+  else {
+
+    $(this).addClass("green");
+
+  }
+
+});
+
 
 //   adding text to the app from local storage
-  $("#userInput-9").val(localStorage.getItem("9 AM"));
-  $("#userInput-10").val(localStorage.getItem("10 AM"));
-  $("#userInput-11").val(localStorage.getItem("11 AM"));
-  $("#userInput-12").val(localStorage.getItem("12 PM"));
-  $("#userInput-13").val(localStorage.getItem("1 PM"));
-  $("#userInput-14").val(localStorage.getItem("2 PM"));
-  $("#userInput-15").val(localStorage.getItem("3 PM"));
-  $("#userInput-16").val(localStorage.getItem("4 PM"));
-  $("#userInput-17").val(localStorage.getItem("5 PM"));
-  
-  $("#local-time").html(moment().format( 'llll'));
+$("#userInput-9").val(localStorage.getItem("9 AM"));
+$("#userInput-10").val(localStorage.getItem("10 AM"));
+$("#userInput-11").val(localStorage.getItem("11 AM"));
+$("#userInput-12").val(localStorage.getItem("12 PM"));
+$("#userInput-13").val(localStorage.getItem("1 PM"));
+$("#userInput-14").val(localStorage.getItem("2 PM"));
+$("#userInput-15").val(localStorage.getItem("3 PM"));
+$("#userInput-16").val(localStorage.getItem("4 PM"));
+$("#userInput-17").val(localStorage.getItem("5 PM"));
+
+$("#local-time").html(moment().format('llll'));
   // console.log(moment());
