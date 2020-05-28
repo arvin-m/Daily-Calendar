af09644b5ca5a4364a7187cdbb195d46c1f21909
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-05-28 01:50:21 -0400

    adding live link to read me file and format the code

--- script.js
+++ script.js
@@ -1,50 +1,27 @@
-
-
 $(".saveBtn").on("click", save);
-
 function save() {
   var value = $(this)
     .siblings(".form")
     .val();
-
-
-
-
   var time = $(this)
     .siblings(".time")
     .text();
-
-
-
   localStorage.setItem(time, value);
-
-
-
-
 };
 
 // chaneg background color for each box base of local time
 $(".row").each(function () {
 
   if (parseInt($(this).attr("id")) < moment().hours()) {
-
     $(this).addClass("red");
   }
   else if (parseInt($(this).attr("id")) === moment().hours()) {
-
     $(this).addClass("gray");
-
-
   }
   else {
-
     $(this).addClass("green");
-
   }
-
 });
-
-
 //   adding text to the app from local storage
 $("#userInput-9").val(localStorage.getItem("9 AM"));
 $("#userInput-10").val(localStorage.getItem("10 AM"));
@@ -55,6 +32,5 @@ $("#userInput-14").val(localStorage.getItem("2 PM"));
 $("#userInput-15").val(localStorage.getItem("3 PM"));
 $("#userInput-16").val(localStorage.getItem("4 PM"));
 $("#userInput-17").val(localStorage.getItem("5 PM"));
-
 $("#local-time").html(moment().format('llll'));
   // console.log(moment());
